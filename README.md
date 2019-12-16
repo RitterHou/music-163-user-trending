96a28c3f1eb665660346f0d6bd0fb222975bd650
侯锐 <hourui@qianmi.com>
2019-12-16 17:22:21 +0800

    添加cron库

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ### How to use
 
 1. 安装nodejs和yarn环境，参考：<https://linuxize.com/post/how-to-install-node-js-on-centos-7/>
-2. 
+2. 下载当前项目到本地，在项目根目录执行 `yarn` 命令来安装依赖
+3. 使用命令 `./run.sh` 执行程序并获取数据，数据会被写到 `data.txt` 文件中
+4. 目前数据是每分钟获取一次，可以根据需要自行修改，定时任务使用到了[cron](https://www.npmjs.com/package/cron)库
--- main.js
+++ main.js
@@ -1,48 +1,52 @@
 const puppeteer = require('puppeteer');
+const CronJob = require('cron').CronJob;
 
-const url = 'https://music.163.com/#/user/songs/rank?id=400271789';
+new CronJob('0 * * * * *', function () {
+    const url = 'https://music.163.com/#/user/songs/rank?id=400271789';
 
-(async () => {
-    const browser = await puppeteer.launch({
-        // headless: false,
-        // slowMo: 250
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    // await page.setViewport({ width: 1920, height: 1080 });
-    // await page.screenshot({ path: 'example.png' });
-    // await page.waitFor(10000000)
+    (async () => {
+        const browser = await puppeteer.launch({
+            // headless: false,
+            // slowMo: 250
+        });
+        const page = await browser.newPage();
+        await page.goto(url);
+        // await page.setViewport({ width: 1920, height: 1080 });
+        // await page.screenshot({ path: 'example.png' });
+        // await page.waitFor(10000000)
 
-    // 数据的内容被存放在iframe中
-    const iframeElement = await page.$('iframe');
-    const iframe = await iframeElement.contentFrame();
+        // 数据的内容被存放在iframe中
+        const iframeElement = await page.$('iframe');
+        const iframe = await iframeElement.contentFrame();
 
-    const result = await iframe.evaluate(() => {
-        let musics = document.querySelectorAll('#m-record .j-flag ul li');
-        let data = [];
-        for (let music of musics) {
-            let a = music.querySelector('.tt .ttc .txt a')
-            let href = a.getAttribute('href');
-            let id = href.split('id=')[1];
-            let name = a.innerText;
-            data.push({ 'id': id, 'name': name });
+        const result = await iframe.evaluate(() => {
+            let musics = document.querySelectorAll('#m-record .j-flag ul li');
+            let data = [];
+            for (let music of musics) {
+                let a = music.querySelector('.tt .ttc .txt a')
+                let href = a.getAttribute('href');
+                let id = href.split('id=')[1];
+                let name = a.innerText;
+                data.push({ 'id': id, 'name': name });
+            }
+            return data;
+        });
+
+        await browser.close();
+
+        console.log(JSON.stringify({ 'time': currentTime(), 'data': result, 'total': result.length }));
+    })();
+
+    function currentTime() {
+        function format(value) {
+            if (value < 10) {
+                value = '0' + value;
+            }
+            return value;
         }
-        return data;
-    });
-
-    await browser.close();
-
-    console.log(JSON.stringify({ 'time': currentTime(), 'data': result, 'total': result.length }));
-})();
-
-function currentTime() {
-    function format(value) {
-        if (value < 10) {
-            value = '0' + value;
-        }
-        return value;
+        let now = new Date();
+        return now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
+            + ' ' + format(now.getHours()) + ':' + format(now.getMinutes()) + ':' + format(now.getSeconds());
     }
-    let now = new Date();
-    return now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
-        + ' ' + format(now.getHours()) + ':' + format(now.getMinutes()) + ':' + format(now.getSeconds());
-}
+
+}, null, true, 'Asia/Shanghai');
